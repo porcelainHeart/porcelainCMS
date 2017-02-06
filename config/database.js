204872319c51b52bf8e15cf99dc4dfbb43388c2f
porcelainHeart <982503660@qq.com>
2017-02-06 16:06:29 +0800

    cms

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': ''
+    },
+	'database': 'my_login',
+    'users_table': 'users'
+};
